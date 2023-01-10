d981b4a8bba1d375eeb0d1704287f1d8ddd2b1ab
alessia.di mario <alessia.di mario@11info2.maxwell.local>
2023-01-10 11:09:57 +0100

    carico

---
+++ src/progimpiccato/ProgImpiccato.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package progimpiccato;
+
+/**
+ *
+ * @author aledm
+ */
+public class ProgImpiccato {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
